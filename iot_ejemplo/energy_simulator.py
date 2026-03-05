270ffbccaf0072165291110e44d9cee46148adb0
Facuu13 <facundoo30@gmail.com>
2026-03-05 14:24:14 -0300

    simular consumo electrico

---
+++ iot_ejemplo/energy_simulator.py
@@ -0,0 +1,27 @@
+import random
+import pandas as pd
+
+data = []
+
+for hour in range(24):
+    
+    if hour < 6:
+        consumption = random.uniform(0.2, 0.4)
+
+    elif hour < 12:
+        consumption = random.uniform(0.4, 0.8)
+
+    elif hour < 18:
+        consumption = random.uniform(0.8, 1.5)
+
+    else:
+        consumption = random.uniform(1.2, 2.5)
+
+    data.append({
+        "hour": hour,
+        "consumption_kwh": round(consumption, 2)
+    })
+
+df = pd.DataFrame(data)
+
+print(df)
